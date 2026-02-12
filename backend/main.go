3339ac376ce87cd6173b1327b08db74f1dc3e975
Vishwa Kalal <vishwa_kalal@Vishwas-MacBook-Air.local>
2026-02-12 14:54:06 -0500

    configured gorilla/mux and Go for the REST API and front to backend communication

--- backend/main.go
+++ backend/main.go
@@ -10,12 +10,14 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-
-	r.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		_ = json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-	}).Methods(http.MethodGet)
-
+	r.HandleFunc("/api/health", healthHandler).Methods(http.MethodGet)
 	log.Println("API server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(map[string]bool{
+		"ok": true,
+	})
 }
