5cebd46724889a0f49db26d0ddb829edc6f07b32
drosabal <dmrosabal@gmail.com>
2026-02-16 18:04:48 -0500

    Created mock data for games and a test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+import SteamAppDisplay from './components/Test/RandomGameDisplay.tsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -17,13 +15,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <SteamAppDisplay />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/Test/RandomGameDisplay.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import type { SteamApps } from '../../mock-data/SteamApps.ts'
+import steamAppsJson from '../../mock-data/steam-apps.json'
+
+const steamApps: SteamApps = steamAppsJson as SteamApps
+
+function SteamAppDisplay() {
+  const [appID, setAppID] = useState(-1)
+
+  const displayRandomGame = () => {
+    const randomIndex = Math.floor(Math.random() * steamApps.app_count)
+    const keys = Object.keys(steamApps.apps)
+    const randomAppID = parseInt(keys[randomIndex])
+    setAppID(randomAppID)
+  }
+
+  return (
+    <>
+      {appID != -1 &&
+        <>
+            <img src={"https://steamcdn-a.akamaihd.net/steam/apps/" + appID + "/header.jpg"} />
+            <p>appid: {appID}</p>
+            <p>initial_price: {steamApps.apps[appID].initial_price}</p>
+            <p>current_price: {steamApps.apps[appID].current_price}</p>
+            <p>name: {steamApps.apps[appID].name}</p>
+            <p>desc: {steamApps.apps[appID].desc}</p>
+            <p>release_date: {steamApps.apps[appID].release_date}</p>
+            <p>release_date_unix: {steamApps.apps[appID].release_date_unix}</p>
+            <p>review_count: {steamApps.apps[appID].review_count}</p>
+            <p>review_desc: {steamApps.apps[appID].review_desc}</p>
+            <p>review_percentage: {steamApps.apps[appID].review_percentage}</p>
+            <p>review_sample: {steamApps.apps[appID].review_sample}</p>
+        </>
+      }
+      <button onClick={displayRandomGame}>Random Game (front-end mock data)</button>
+    </>
+  )
+}
+
+export default SteamAppDisplay
