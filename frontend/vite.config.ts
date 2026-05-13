e5f2d1f04feda6088f94a0569671dd1163d54614
Liam McKenna <lee@liamdouglas.com>
2026-05-13 14:03:27 -0400

    changse

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SteamQuack/',
   plugins: [react()],
   // Add this block to force a single version of React
   resolve: {
